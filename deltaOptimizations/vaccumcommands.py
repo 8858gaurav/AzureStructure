b43c2216d0936738e58fb78c9de9aec524848cd8
Gaurav Mishra <52245439+8858gaurav@users.noreply.github.com>
2025-09-04 15:01:00 +0530

    Update vaccumcommands.py
    vaccum commands operations

--- deltaOptimizations/vaccumcommands.py
+++ deltaOptimizations/vaccumcommands.py
@@ -213,3 +213,98 @@ WHERE order_id = '102'
 # -- 3rd opeartion delete
 # -- order_id	order_date	order_customer_id	order_status
 # -- No records
+
+
+############################### Vaccum Commands ##############################
+
+spark.conf.set("spark.databricks.delta.retentionDurationCheck.enabled", "false")
+
+%sql
+describe history misgauravdb.delta_orders_partitioned ;
+
+# -- version	    3	                  2	                    1	                    0
+# -- timestamp	 2025-09-04T08:08:34Z	2025-09-04T08:02:26Z	2025-09-04T07:54:40Z	2025-09-04T07:39:14Z
+# -- operation	 DELETE	              UPDATE	              WRITE	                CREATE OR REPLACE TABLE AS SELECT
+# -- operationParameters	
+# -- {"predicate":"[\"(order_id#7343 = 102)\"]"}	
+# -- {"predicate":"[\"(order_id#6051 = 101)\"]"}	
+# -- {"mode":"Append","statsOnLoad":"false","partitionBy":"[]"}	
+# -- {"partitionBy":"[]","clusterBy":"[]","description":null,"isManaged":"false","properties":"{\"delta.enableDeletionVectors\":\"true\"}","statsOnLoad":"false"}
+# -- readVersion	2	1	0	null
+# -- isBlindAppend	FALSE	FALSE	TRUE	FALSE
+# -- operationMetrics	
+# -- {"numRemovedFiles":"0","numRemovedBytes":"0","numCopiedRows":"0","numDeletionVectorsAdded":"1","numDeletionVectorsRemoved":"1","numAddedChangeFiles":"0","executionTimeMs":"2852","numDeletionVectorsUpdated":"1","numDeletedRows":"1","scanTimeMs":"1469","numAddedFiles":"0","numAddedBytes":"0","rewriteTimeMs":"1382"}	{"numRemovedFiles":"0","numRemovedBytes":"0","numCopiedRows":"0","numDeletionVectorsAdded":"1","numDeletionVectorsRemoved":"0","numAddedChangeFiles":"0","executionTimeMs":"6253","numDeletionVectorsUpdated":"0","scanTimeMs":"3736","numAddedFiles":"1","numUpdatedRows":"1","numAddedBytes":"1559","rewriteTimeMs":"2489"}	{"numFiles":"1","numOutputRows":"1","numOutputBytes":"1637"}	
+# -- {"numFiles":"1","numRemovedFiles":"0","numRemovedBytes":"0","numOutputRows":"104","numOutputBytes":"2814"}
+
+%sql
+vacuum misgauravdb.delta_orders_partitioned retain 1 hours dry run
+# -- No rows returned
+
+%sql
+vacuum misgauravdb.delta_orders_partitioned retain 1 hours;
+# -- path
+# -- dbfs:/mnt/reaildb/delta_tables
+
+%sql
+select * from misgauravdb.delta_orders_partitioned version as of 0 limit 5;
+# -- No results
+
+%sql
+describe history misgauravdb.delta_orders_partitioned
+
+# -- version	7	6	5	4	3	2	1	0		
+# -- timestamp	2025-09-04T09:17:30Z	2025-09-04T09:17:25Z	2025-09-04T09:12:20Z	2025-09-04T09:12:15Z	2025-09-04T08:08:34Z	2025-09-04T08:02:26Z	2025-09-04T07:54:40Z	2025-09-04T07:39:14Z		
+# -- operation	VACUUM END	VACUUM START	VACUUM END	VACUUM START	DELETE	UPDATE	WRITE	CREATE OR REPLACE TABLE AS SELECT		
+# -- operationParameters	
+# -- {"status":"COMPLETED"}	
+# -- {"retentionCheckEnabled":"false","defaultRetentionMillis":"604800000","specifiedRetentionMillis":"3600000"}	{"status":"COMPLETED"}	
+# -- {"retentionCheckEnabled":"false","defaultRetentionMillis":"604800000","specifiedRetentionMillis":"3600000"}	
+# -- {"predicate":"[\"(order_id#7343 = 102)\"]"}	
+# -- {"predicate":"[\"(order_id#6051 = 101)\"]"}	
+# -- {"mode":"Append","statsOnLoad":"false","partitionBy":"[]"}	
+# -- {"partitionBy":"[]","clusterBy":"[]","description":null,"isManaged":"false","properties":"{\"delta.enableDeletionVectors\":\"true\"}","statsOnLoad":"false"}		
+
+# -- isBlindAppend	TRUE	TRUE	TRUE	TRUE	FALSE	FALSE	TRUE	FALSE		
+# -- operationMetrics	
+# -- {"numDeletedFiles":"0","numVacuumedDirectories":"1"}	
+# -- {"numFilesToDelete":"0","sizeOfDataToDelete":"0"}	
+# -- {"numDeletedFiles":"1","numVacuumedDirectories":"1"}	
+# -- {"numFilesToDelete":"1","sizeOfDataToDelete":"43"}
+
+# -- {"numRemovedFiles":"0","numRemovedBytes":"0","numCopiedRows":"0","numDeletionVectorsAdded":"1","numDeletionVectorsRemoved":"1","numAddedChangeFiles":"0","executionTimeMs":"2852","numDeletionVectorsUpdated":"1","numDeletedRows":"1","scanTimeMs":"1469","numAddedFiles":"0","numAddedBytes":"0","rewriteTimeMs":"1382"}
+
+# -- {"numRemovedFiles":"0","numRemovedBytes":"0","numCopiedRows":"0","numDeletionVectorsAdded":"1","numDeletionVectorsRemoved":"0","numAddedChangeFiles":"0","executionTimeMs":"6253","numDeletionVectorsUpdated":"0","scanTimeMs":"3736","numAddedFiles":"1","numUpdatedRows":"1","numAddedBytes":"1559","rewriteTimeMs":"2489"}
+
+# -- {"numFiles":"1","numOutputRows":"1","numOutputBytes":"1637"}	
+# -- {"numFiles":"1","numRemovedFiles":"0","numRemovedBytes":"0","numOutputRows":"104","numOutputBytes":"2814"}		
+
+
+dbutils.fs.ls('mnt/reaildb/delta_tables/_delta_log')
+# 10 json file will be created: 3 for operations, 1 for table creations
+# 6 for vaccum ( 3 for vaccum start operations, and 3 for vaccum end operations)
+
+# [FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000000.crc', name='00000000000000000000.crc', size=3174, modificationTime=1756971555000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000000.json', name='00000000000000000000.json', size=2263, modificationTime=1756971554000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000001.00000000000000000006.compacted.json', name='00000000000000000001.00000000000000000006.compacted.json', size=4099, modificationTime=1756977446000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000001.crc', name='00000000000000000001.crc', size=3935, modificationTime=1756972480000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000001.json', name='00000000000000000001.json', size=1372, modificationTime=1756972480000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000002.crc', name='00000000000000000002.crc', size=4796, modificationTime=1756972947000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000002.json', name='00000000000000000002.json', size=3303, modificationTime=1756972946000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000003.crc', name='00000000000000000003.crc', size=4796, modificationTime=1756973314000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000003.json', name='00000000000000000003.json', size=2675, modificationTime=1756973314000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000004.crc', name='00000000000000000004.crc', size=4796, modificationTime=1756977135000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000004.json', name='00000000000000000004.json', size=585, modificationTime=1756977135000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000005.crc', name='00000000000000000005.crc', size=4796, modificationTime=1756977141000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000005.json', name='00000000000000000005.json', size=506, modificationTime=1756977140000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000006.crc', name='00000000000000000006.crc', size=4796, modificationTime=1756977445000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000006.json', name='00000000000000000006.json', size=584, modificationTime=1756977445000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000007.crc', name='00000000000000000007.crc', size=4796, modificationTime=1756977451000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000007.json', name='00000000000000000007.json', size=506, modificationTime=1756977450000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000008.crc', name='00000000000000000008.crc', size=4796, modificationTime=1756977947000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000008.json', name='00000000000000000008.json', size=584, modificationTime=1756977947000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000009.crc', name='00000000000000000009.crc', size=4796, modificationTime=1756977952000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/00000000000000000009.json', name='00000000000000000009.json', size=506, modificationTime=1756977952000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/__tmp_path_dir/', name='__tmp_path_dir/', size=0, modificationTime=0),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/_last_vacuum_info', name='_last_vacuum_info', size=50, modificationTime=1756977952000),
+#  FileInfo(path='dbfs:/mnt/reaildb/delta_tables/_delta_log/_staged_commits/', name='_staged_commits/', size=0, modificationTime=0)]
+
